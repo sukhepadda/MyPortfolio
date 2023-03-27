85ff6f4ac6982139fc65c936943eefa8b244b769
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-27 23:01:09 +0530

    on going

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Typewriter from 'typewriter-effect';
-import {BsArrowUpRight} from "react-icons/bs"
+import { BsArrowUpRight, BsChevronDown } from "react-icons/bs"
+import me from "../assets/me.png"
 
 function Home() {
     const animations = {
@@ -40,10 +41,10 @@ function Home() {
                     <Typewriter options={{
                         strings: ["A Developer", "A Designer", "A Creator"],
                         autoStart: true,
-                        loop:true,
-                        cursor:"",
+                        loop: true,
+                        cursor: "",
                         wrapperClassName: "typewriterpara",
-                    }} 
+                    }}
                     />
 
                     <div>
@@ -55,9 +56,31 @@ function Home() {
                         </a>
                     </div>
 
+                    <article>
+                        <p>+
+                            <span>100</span></p>
+                        <span>Client WorldWide</span>
+                    </article>
+
+                    <aside>
+                        <article>
+                            <p>+
+                                <span>500</span></p>
+                            <span>Projects Made</span>
+                        </article>
+                    </aside>
+                    <article data-special >
+                        <p>Contact</p>
+                        <span>paddasukhwantsingh@gmail.com</span>
+                        <span>Client WorldWide</span>
+                    </article>
+
                 </div>
             </section>
-            <section></section>
+            <section>
+                <img src={me} alt="Sukhwant" />
+            </section>
+            <BsChevronDown />
         </div>
     )
 }
